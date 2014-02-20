0086906a68576ba9d319dfabe77eab1ae4863ffc
Cameron MacFarland <distantcam@gmail.com>
2014-02-20 14:21:38 +0800

    Upgraded to 2.0

--- src/main.c
+++ src/main.c
@@ -1,146 +1,133 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 
+Window *window;
 
-#define MY_UUID { 0x00, 0xB3, 0xC1, 0xF8, 0x31, 0xE6, 0x4E, 0x9A, 0x8E, 0x16, 0xFA, 0xB2, 0x18, 0xCF, 0x19, 0x83 }
-PBL_APP_INFO(MY_UUID,
-             "Bar Graph", "Cameron MacFarland",
-             1, 2, /* App version */
-             RESOURCE_ID_IMAGE_MENU_ICON,
-             APP_INFO_STANDARD_APP);
+GBitmap *faceImage;
+BitmapLayer *faceLayer;
 
-Window window;
+Layer *hourLayer;
+Layer *minLayer;
 
-BmpContainer faceImage;
-
-Layer hourLayer;
-Layer minLayer;
-
-TextLayer dateText;
-
+TextLayer *dateLayer;
 
 void update_hour(Layer *me, GContext* ctx) {
-  PblTm t;
+	time_t now = time(NULL);
+  	struct tm *t = localtime(&now);
 
-  get_time(&t);
+  	int height = t->tm_hour;
 
-  int height = t.tm_hour;
+  	if (height > 12)
+    	height -= 12;
 
-  if (height > 12)
-    height -= 12;
+  	if (height == 0)
+    	height = 12;
 
-  if (height == 0)
-    height = 12;
+  	graphics_context_set_fill_color(ctx, GColorWhite);
+  	graphics_context_set_stroke_color(ctx, GColorBlack);
 
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_context_set_stroke_color(ctx, GColorBlack);
+  	graphics_fill_rect(ctx, GRect(38, 132 - height * 10, 30, height * 10), 0, GCornerNone);
 
-  graphics_fill_rect(ctx, GRect(38, 132 - height * 10, 30, height * 10), 0, GCornerNone);
-
-  for (int i = 0; i < height-1; i++)
-  {
-    graphics_draw_line(ctx, GPoint(38, 122 - i*10), GPoint(67, 122 - i*10));
-  }
+  	for (int i = 0; i < height-1; i++)
+	{
+    	graphics_draw_line(ctx, GPoint(38, 122 - i*10), GPoint(67, 122 - i*10));
+	}
 }
-
 
 void update_minute(Layer *me, GContext* ctx) {
-  PblTm t;
+	time_t now = time(NULL);
+  	struct tm *t = localtime(&now);
 
-  get_time(&t);
+    int height = t->tm_min * 2;
 
-  int height = t.tm_min * 2;
+    graphics_context_set_fill_color(ctx, GColorWhite);
+    graphics_context_set_stroke_color(ctx, GColorBlack);
 
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_context_set_stroke_color(ctx, GColorBlack);
+    graphics_fill_rect(ctx, GRect(76, 132 - height, 30, height), 0, GCornerNone);
 
-  graphics_fill_rect(ctx, GRect(76, 132 - height, 30, height), 0, GCornerNone);
-
-  for (int i = 10; i < height; i+=10)
-  {
-    graphics_draw_line(ctx, GPoint(76, 132 - i), GPoint(105, 132 - i));
-  }
+    for (int i = 10; i < height; i+=10)
+    {
+      graphics_draw_line(ctx, GPoint(76, 132 - i), GPoint(105, 132 - i));
+    }
 }
-
 
 void update_date()
 {
-  PblTm t;
-
-  get_time(&t);
-
-  static char date_text[] = "xxx, xxx 00";
-
-  string_format_time(date_text, sizeof(date_text), "%a, %b %e", &t);
-  text_layer_set_text(&dateText, date_text);
+	time_t now = time(NULL);
+  	struct tm *t = localtime(&now);
+	
+	static char dateText[] = "xxx, xxx 00";
+	
+	strftime(dateText, sizeof(dateText), "%a, %b %e", t);
+	text_layer_set_text(dateLayer, dateText);
 }
 
-
-void handle_tick(AppContextRef ctx, PebbleTickEvent *t) {
-  (void)ctx;
-  (void)t;
-
-  if ((t->units_changed & HOUR_UNIT) != 0) {
-    layer_mark_dirty(&hourLayer);
-  }
-
-  if ((t->units_changed & MINUTE_UNIT) != 0) {
-    layer_mark_dirty(&minLayer);
-  }
-
-  if ((t->units_changed & DAY_UNIT) != 0) {
-    update_date();
-  }
+void handle_hour_tick(struct tm *tick_time, TimeUnits units_changed) {
+	layer_mark_dirty(hourLayer);
 }
 
-
-void handle_init(AppContextRef ctx) {
-  (void)ctx;
-
-  window_init(&window, "Bar Graph");
-  window_stack_push(&window, true /* Animated */);
-
-  resource_init_current_app(&APP_RESOURCES);
-
-  bmp_init_container(RESOURCE_ID_IMAGE_FACE, &faceImage);
-  layer_add_child(&window.layer, &faceImage.layer.layer);
-
-  layer_init(&hourLayer, window.layer.frame);
-  hourLayer.update_proc = &update_hour;
-  layer_add_child(&window.layer, &hourLayer);
-
-  layer_init(&minLayer, window.layer.frame);
-  minLayer.update_proc = &update_minute;
-  layer_add_child(&window.layer, &minLayer);
-
-  text_layer_init(&dateText, GRect(0,138,144,30));
-  text_layer_set_text_color(&dateText, GColorWhite);
-  text_layer_set_background_color(&dateText, GColorClear);
-  text_layer_set_text_alignment(&dateText, GTextAlignmentCenter);
-  text_layer_set_font(&dateText, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ARIAL_22)));
-  layer_add_child(&window.layer, &dateText.layer);
-
-  update_date();
+void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+	layer_mark_dirty(minLayer);
 }
 
-
-void handle_deinit(AppContextRef ctx) {
-  (void)ctx;
-
-  bmp_deinit_container(&faceImage);
+void handle_day_tick(struct tm *tick_time, TimeUnits units_changed) {
+	update_date();
 }
 
+void init(void) {
+	
+	// Set up window
+	window = window_create();
+	window_set_background_color(window, GColorBlack);
+  	window_stack_push(window, true); // Animated
+	
+	Layer *windowLayer = window_get_root_layer(window);
+	GRect windowFrame = layer_get_frame(windowLayer);
+	
+	// Set up face
+	faceImage = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_FACE);
+	faceLayer = bitmap_layer_create(windowFrame);
+	
+	bitmap_layer_set_bitmap(faceLayer, faceImage);
+	layer_add_child(windowLayer, bitmap_layer_get_layer(faceLayer));
+	
+	// Set up hour
+	hourLayer = layer_create(windowFrame);
+	layer_set_update_proc(hourLayer, update_hour);
+  	layer_add_child(windowLayer, hourLayer);
+	
+	// Set up minute
+	minLayer = layer_create(windowFrame);
+	layer_set_update_proc(minLayer, update_minute);
+  	layer_add_child(windowLayer, minLayer);
+	
+	// Set up date text
+	dateLayer = text_layer_create(GRect(0,138,144,30));
+	text_layer_set_text_color(dateLayer, GColorWhite);
+  	text_layer_set_background_color(dateLayer, GColorClear);
+  	text_layer_set_text_alignment(dateLayer, GTextAlignmentCenter);
+  	text_layer_set_font(dateLayer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ARIAL_22)));
+  	layer_add_child(windowLayer, text_layer_get_layer(dateLayer));
+	
+	tick_timer_service_subscribe(HOUR_UNIT, handle_hour_tick);
+	tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+	tick_timer_service_subscribe(DAY_UNIT, handle_day_tick);
+}
 
-void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init,
-	.deinit_handler = &handle_deinit,
+void deinit(void) {
+	layer_remove_from_parent(bitmap_layer_get_layer(faceLayer));
+  	bitmap_layer_destroy(faceLayer);
+  	gbitmap_destroy(faceImage);
+	
+	layer_destroy(hourLayer);
+	layer_destroy(minLayer);
+	
+	text_layer_destroy(dateLayer);
+	
+	window_destroy(window);
+}
 
-    .tick_info = {
-      .tick_handler = &handle_tick,
-      .tick_units = MINUTE_UNIT
-    }
-  };
-  app_event_loop(params, &handlers);
+int main(void) {
+	init();
+	app_event_loop();
+	deinit();
 }
